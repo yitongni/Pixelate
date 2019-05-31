e78f903b2cf342971b45afd9d924634d34860542
yitongni <31358391+yitongni@users.noreply.github.com>
2019-05-31 12:14:05 -0400

    Rows and columns working

--- pixel.js
+++ pixel.js
@@ -5,33 +5,15 @@ function addRow() {
 
     var cells = table.rows[0].cells;
 
-
-    //let td = table.getElementsByTagName("td");
-    // let td = table.getElementsByTagName("td");
     //for each column - add a row
     for (let i = 1; i < cells.length; i++) {
         row.insertCell(0);
     }
+}
 
-
-    // var tr = table.insertRow(table.size);
-    // let td = table.getElementsByTagName("td");
-
-    // if (tr.length === 0) {
-    //     var cell1 = tr.insertCell(0);
-    //     cell1.bgColor = "Blue"
-    //     cell1.style.width = "50px"
-    //     cell1.style.height = "50px"
-    // }
-    // else {
-    //     for (let i = 0; i < td.length; i++) {
-    //         var cell1 = tr.insertCell(0);
-    //         cell1.bgColor = "Blue"
-    //         cell1.style.width = "50px"
-    //         cell1.style.height = "50px"
-    //     }
-    // }
-
+function deleteRow(){
+    let table=document.getElementById("table");
+    var row = table.deleteRow(table.rows.length-1);
 }
 
 function addColumn() {
