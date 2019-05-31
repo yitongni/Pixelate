4f118d77bc4bec6f9e68bde90f15d74440056b6a
yitongni <31358391+yitongni@users.noreply.github.com>
2019-05-31 12:00:56 -0400

    Styling

--- pixel.js
+++ pixel.js
@@ -3,9 +3,6 @@ function addRow(){
     let table=document.getElementById("table");
     var row = table.insertRow(table.rows.length);
     var cell1 = row.insertCell(0);
-    cell1.bgColor="Blue"
-    cell1.style.width="50px"
-    cell1.style.height="50px"
 }
 
 function deleteRow(){
