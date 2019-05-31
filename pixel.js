bfa82d29c647bbdea9103056de40ae6cba548e29
yitongni <31358391+yitongni@users.noreply.github.com>
2019-05-31 16:04:34 -0400

    Can hover but must fix

--- pixel.js
+++ pixel.js
@@ -1,8 +1,31 @@
+function changeColorOnHover(){
+    let table = document.getElementById("table");
+    for (let i = 0; i < table.rows.length; i++) {
+        for(let j=0; j<table.rows[i].cells.length; j++){
+            let cell = table.rows[i].cells[j];
+            cell.addEventListener("mouseover", function () { cell.style.backgroundColor = "yellow"; });
+        }
+    }
+}
+
+function removeOnHover(){
+    let table = document.getElementById("table");
+    for (let i = 0; i < table.rows.length; i++) {
+        for(let j=0; j<table.rows[i].cells.length; j++){
+            let cell = table.rows[i].cells[j];
+            cell.removeEventListener("mouseover", function () {});
+        }
+    }
+}
+
 function addRow() {
     let table = document.getElementById("table");
     let row = table.insertRow(table.size);
     let icell = row.insertCell(0);
     icell.addEventListener("click", function () { icell.style.backgroundColor = "yellow"; });
+    //icell.addEventListener("mouseover", function () { icell.style.backgroundColor = "yellow"; });
+    icell.addEventListener("mousedown", function () {changeColorOnHover()});
+    icell.addEventListener("mouseup", function () {removeOnHover()});
 
     let cells = table.rows[0].cells;
 
@@ -10,6 +33,9 @@ function addRow() {
     for (let i = 1; i < cells.length; i++) {
         let cell = row.insertCell(0);
         cell.addEventListener("click", function () { cell.style.backgroundColor = "yellow"; });
+        //cell.addEventListener("mouseover", function () { cell.style.backgroundColor = "yellow"; });
+        cell.addEventListener("mousedown", function () {changeColorOnHover()});
+        cell.addEventListener("mouseup", function () {removeOnHover()});
     }
 }
 
@@ -25,8 +51,10 @@ function addColumn() {
         let tr = table.rows[i];
         let cell = tr.insertCell(tr.length);
         cell.addEventListener("click", function () { cell.style.backgroundColor = "yellow"; });
+        //cell.addEventListener("mouseover", function () { cell.style.backgroundColor = "yellow"; });
+        cell.addEventListener("mousedown", function () {changeColorOnHover()});
+        cell.addEventListener("mouseup", function () {removeOnHover()});
     }
-
 }
 
 function deleteColumn() {
@@ -36,3 +64,28 @@ function deleteColumn() {
         tr.deleteCell(tr.length - 1);
     }
 }
+
+function restoreCellsToOriginalColor(){
+    console.log("Restoring");
+    let table = document.getElementById("table");
+    for (let i = 0; i < table.rows.length; i++) {
+       
+        for(let j=0; j<table.rows[i].cells.length; j++){
+            let cell = table.rows[i].cells[j];
+            cell.style.backgroundColor="rgb(27, 150, 152)";
+        }
+    }
+}
+
+function fillCellsWithSelectedColor(){
+    console.log("Filling");
+    let table = document.getElementById("table");
+    for (let i = 0; i < table.rows.length; i++) {
+       
+        for(let j=0; j<table.rows[i].cells.length; j++){
+            let cell = table.rows[i].cells[j];
+            cell.style.backgroundColor="yellow";
+        }
+    }
+}
+
