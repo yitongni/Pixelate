dbbf9a8cb733c13c2e8fd363ff6d327a121c9b11
yitongni <31358391+yitongni@users.noreply.github.com>
2019-06-03 00:49:41 -0400

    fill in uncolored cells

--- pixel.js
+++ pixel.js
@@ -1,46 +1,52 @@
-function changeColorOfCell(cell){
+var myfunc = function(cell){
     cell.style.backgroundColor = document.getElementById("Colors").value;
 }
-
 
 function changeColorOnHover(){
     let table = document.getElementById("table");
     for (let i = 0; i < table.rows.length; i++) {
         for(let j=0; j<table.rows[i].cells.length; j++){
             let cell = table.rows[i].cells[j];
-            cell.addEventListener("mouseover", function () { changeColorOfCell(cell) });
+            cell.addEventListener("mouseover", function _listener(){
+                myfunc(cell);
+
+                cell.removeEventListener("mouseover", _listener, true ); 
+            },
+            true);
         }
     }
 }
 
-function removeOnHover(){
-    let table = document.getElementById("table");
-    for (let i = 0; i < table.rows.length; i++) {
-        for(let j=0; j<table.rows[i].cells.length; j++){
-            let cell = table.rows[i].cells[j];
-            cell.removeEventListener("mouseover", function () {});
-        }
-    }
-}
+// function removeOnHover(){
+//     console.log("Mouse up");
+//     let table = document.getElementById("table");
+//     for (let i = 0; i < table.rows.length; i++) {
+//         for(let j=0; j<table.rows[i].cells.length; j++){
+//             let cell = table.rows[i].cells[j];
+//             cell.removeEventListener("mouseover", myfunc(cell), true );
+//         }
+//     }
+// }
 
 function addRow() {
     let table = document.getElementById("table");
     let row = table.insertRow(table.size);
     let icell = row.insertCell(0);
-    icell.addEventListener("click", function () { changeColorOfCell(icell) });
-    //icell.addEventListener("mouseover", function () { icell.style.backgroundColor = "yellow"; });
-    icell.addEventListener("mousedown", function () {changeColorOnHover()});
-    icell.addEventListener("mouseup", function () {removeOnHover()});
+    icell.addEventListener("mousedown", function(){ changeColorOnHover() });
+    //icell.addEventListener("mouseup", function(){ removeOnHover() });
+    icell.addEventListener("click", function (){ myfunc(icell) });
+    
 
     let cells = table.rows[0].cells;
 
     //for each column - add a row
     for (let i = 1; i < cells.length; i++) {
         let cell = row.insertCell(0);
-        cell.addEventListener("click", function () { changeColorOfCell(cell) });
-        //cell.addEventListener("mouseover", function () { cell.style.backgroundColor = "yellow"; });
-        cell.addEventListener("mousedown", function () {changeColorOnHover()});
-        cell.addEventListener("mouseup", function () {removeOnHover()});
+        cell.addEventListener("mousedown", function(){ changeColorOnHover() });
+        //cell.addEventListener("mouseup", function(){ removeOnHover() });
+
+        cell.addEventListener("click", function () { myfunc(cell) });
+        
     }
 }
 
@@ -55,10 +61,10 @@ function addColumn() {
     for (let i = 0; i < table.rows.length; i++) {
         let tr = table.rows[i];
         let cell = tr.insertCell(tr.length);
-        cell.addEventListener("click", function () { changeColorOfCell(icell) });
-        //cell.addEventListener("mouseover", function () { cell.style.backgroundColor = "yellow"; });
-        cell.addEventListener("mousedown", function () {changeColorOnHover()});
-        cell.addEventListener("mouseup", function () {removeOnHover()});
+        cell.addEventListener("mousedown", function(){ changeColorOnHover() });
+        //cell.addEventListener("mouseup", function(){ removeOnHover() });
+        cell.addEventListener("click", function () { myfunc(cell) });
+        
     }
 }
 
@@ -89,7 +95,20 @@ function fillCellsWithSelectedColor(){
        
         for(let j=0; j<table.rows[i].cells.length; j++){
             let cell = table.rows[i].cells[j];
-            cell.style.backgroundColor="yellow";
+            myfunc(cell);
+        }
+    }
+}
+
+function fillUncoloredCellsWithSelectedColor(){
+    console.log("Filling");
+    let table = document.getElementById("table");
+    for (let i = 0; i < table.rows.length; i++) {
+        for(let j=0; j<table.rows[i].cells.length; j++){
+            let cell = table.rows[i].cells[j];
+            if(cell.style.backgroundColor==="rgb(27, 150, 152)"){
+                myfunc(cell);
+            }
         }
     }
 }
